64f257b602159cadc00489638dd5c479071d14ab
Andriy Mosol <40470899+Mosandr@users.noreply.github.com>
2020-11-22 20:03:13 +0200

    changed longtext script

---
+++ src/js/slider.js
@@ -0,0 +1,9 @@
+$(document).on('ready', function () {
+      
+  $(".testimonial__slider").slick({
+      infinite: true,      
+      slidesToShow: 1,
+      nextArrow: '.testimonials__next-btn',
+      prevArrow: '.testimonials__prev-btn',         
+    });   
+});    
