468f3f3d89a1ab517709e7dc984fb3b20c1bf972
Andriy Mosol <40470899+Mosandr@users.noreply.github.com>
2020-12-21 00:04:29 +0200

    Made slider

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,20 +1,11 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    program: document.querySelector("[program-closed]"),
-    teacher: document.querySelector("[teacher-closed]"),
-    methods: document.querySelector("[methods-closed]"),
-    footer: document.querySelector("[footer-closed]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-  refs.program.addEventListener("click", toggleModal);
-  refs.teacher.addEventListener("click", toggleModal);
-  refs.methods.addEventListener("click", toggleModal);
-  refs.footer.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);  
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,9 +1,8 @@
 $(document).on('ready', function () {
-      
-  $(".testimonial__slider").slick({
-      infinite: true,      
-      slidesToShow: 1,
-      nextArrow: '.testimonials__next-btn',
-      prevArrow: '.testimonials__prev-btn',         
-    });   
-});    
+  $('.testimonial__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    nextArrow: '.testimonials__next-btn',
+    prevArrow: '.testimonials__prev-btn',
+  });
+});
